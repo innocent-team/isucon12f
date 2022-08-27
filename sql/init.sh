aa879da8520b635631f04f86dcfce8d38acdecec
utgwkk <utagawakiki@gmail.com>
2022-08-27 14:57:26 +0900

    各ホストに接続

--- sql/init.sh
+++ sql/init.sh
@@ -14,30 +14,30 @@ function init_db () {
 
 	mysql -u"$ISUCON_DB_USER" \
 			-p"$ISUCON_DB_PASSWORD" \
-			--host "$ISUCON_DB_HOST" \
+			--host "$host" \
 			--port "$ISUCON_DB_PORT" \
 			"$ISUCON_DB_NAME" < 3_schema_exclude_user_presents.sql
 
 	mysql -u"$ISUCON_DB_USER" \
 			-p"$ISUCON_DB_PASSWORD" \
-			--host "$ISUCON_DB_HOST" \
+			--host "$host" \
 			--port "$ISUCON_DB_PORT" \
 			"$ISUCON_DB_NAME" < 4_alldata_exclude_user_presents.sql
 
 	echo "delete from user_presents where id > 100000000000" | mysql -u"$ISUCON_DB_USER" \
 			-p"$ISUCON_DB_PASSWORD" \
-			--host "$ISUCON_DB_HOST" \
+			--host "$host" \
 			--port "$ISUCON_DB_PORT" \
 			"$ISUCON_DB_NAME"
 
-	DIR=`mysql -u"$ISUCON_DB_USER" -p"$ISUCON_DB_PASSWORD" -h "$ISUCON_DB_HOST" -Ns -e "show variables like 'secure_file_priv'" | cut -f2`
+	DIR=`mysql -u"$ISUCON_DB_USER" -p"$ISUCON_DB_PASSWORD" -h "$host" -Ns -e "show variables like 'secure_file_priv'" | cut -f2`
 	SECURE_DIR=${DIR:-/var/lib/mysql-files/}
 
 	ssh $SSH_FLAGS isucon@${ISUCON_DB_HOST} -- "bash -c 'cd $(pwd) && sudo cp 5_user_presents_not_receive_data.tsv ${SECURE_DIR}'"
 
 	echo "LOAD DATA INFILE '${SECURE_DIR}5_user_presents_not_receive_data.tsv' REPLACE INTO TABLE user_presents FIELDS ESCAPED BY '|' IGNORE 1 LINES ;" | mysql -u"$ISUCON_DB_USER" \
 			-p"$ISUCON_DB_PASSWORD" \
-			--host "$ISUCON_DB_HOST" \
+			--host "$host" \
 			--port "$ISUCON_DB_PORT" \
 			"$ISUCON_DB_NAME" 
 }
