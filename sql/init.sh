d00eaad93c556ee2e53b7a64880255ca0b2a3b86
innocent <innocent@example.com>
2022-08-27 02:26:31 +0000

    sql init

--- sql/init.sh
+++ sql/init.sh
@@ -2,11 +2,12 @@
 set -ex
 cd `dirname $0`
 
-ISUCON_DB_HOST=${ISUCON_DB_HOST:-127.0.0.1}
+ISUCON_DB_HOST=${ISUCON_DB_HOST:-isucon2}
 ISUCON_DB_PORT=${ISUCON_DB_PORT:-3306}
 ISUCON_DB_USER=${ISUCON_DB_USER:-isucon}
 ISUCON_DB_PASSWORD=${ISUCON_DB_PASSWORD:-isucon}
 ISUCON_DB_NAME=${ISUCON_DB_NAME:-isucon}
+SSH_FLAGS="-i /home/isucon/.ssh/id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
 
 mysql -u"$ISUCON_DB_USER" \
 		-p"$ISUCON_DB_PASSWORD" \
@@ -29,7 +30,7 @@ echo "delete from user_presents where id > 100000000000" | mysql -u"$ISUCON_DB_U
 DIR=`mysql -u"$ISUCON_DB_USER" -p"$ISUCON_DB_PASSWORD" -h "$ISUCON_DB_HOST" -Ns -e "show variables like 'secure_file_priv'" | cut -f2`
 SECURE_DIR=${DIR:-/var/lib/mysql-files/}
 
-sudo cp 5_user_presents_not_receive_data.tsv ${SECURE_DIR}
+ssh $SSH_FLAGS isucon@${ISUCON_DB_HOST} -- "bash -c 'cd $(pwd) && sudo cp 5_user_presents_not_receive_data.tsv ${SECURE_DIR}'"
 
 echo "LOAD DATA INFILE '${SECURE_DIR}5_user_presents_not_receive_data.tsv' REPLACE INTO TABLE user_presents FIELDS ESCAPED BY '|' IGNORE 1 LINES ;" | mysql -u"$ISUCON_DB_USER" \
         -p"$ISUCON_DB_PASSWORD" \
