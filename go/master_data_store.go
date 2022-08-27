94ed9f181faefd3ca4b7181909f55493cd977f3e
utgwkk <utagawakiki@gmail.com>
2022-08-27 16:24:54 +0900

    キャッシュからListとRefreshまで

--- go/master_data_store.go
+++ go/master_data_store.go
@@ -9,35 +9,56 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-var localGachaMasters = LocalGachaMasters{GachaMaster: map[int]LocalGachaMaster{}}
+var localGachaMasters = LocalGachaMasters{
+	GachaMasterByID:        map[int64]*GachaMaster{},
+	GachaMasters:           []*GachaMaster{},
+	GachaItemListByGachaID: map[int64][]*GachaItemMaster{},
+	GachaItemList:          []*GachaItemMaster{},
+}
 
 type LocalGachaMasters struct {
-	GachaMaster map[int]LocalGachaMaster
-}
-type LocalGachaMaster struct {
-	ID    int64
-	Name  string
-	Items LocalGachaItem
-}
-type LocalGachaItem struct {
-	ItemType int
-	ItemID   int
-	Amount   int
-	Weight   int
+	GachaMasterByID        map[int64]*GachaMaster
+	GachaMasters           []*GachaMaster
+	GachaItemListByGachaID map[int64][]*GachaItemMaster
+	GachaItemList          []*GachaItemMaster
 }
 
-func (l *LocalGachaMasters) Refresh() error {
+// ガチャのマスターデータのキャッシュを更新する
+func (l *LocalGachaMasters) Refresh(c echo.Context, h *Handler) error {
+	ctx := c.Request().Context()
+
+	gachaMasterList := []*GachaMaster{}
+	query := "SELECT * FROM gacha_masters ORDER BY display_order ASC"
+	err := h.DB.SelectContext(ctx, &gachaMasterList, query)
+	if err != nil {
+		return errorResponse(c, http.StatusInternalServerError, err)
+	}
+	l.GachaMasters = gachaMasterList
+	for _, gachaMaster := range gachaMasterList {
+		l.GachaMasterByID[gachaMaster.ID] = gachaMaster
+	}
+
+	var gachaItemList []*GachaItemMaster
+	query = "SELECT * FROM gacha_item_masters ORDER BY id ASC"
+	err = h.DB.SelectContext(ctx, &gachaItemList, query)
+	if err != nil {
+		return errorResponse(c, http.StatusInternalServerError, err)
+	}
+	l.GachaItemList = gachaItemList
+	for _, gachaItem := range gachaItemList {
+		l.GachaItemListByGachaID[gachaItem.ID] = append(l.GachaItemListByGachaID[gachaItem.ID], gachaItem)
+	}
+
 	return nil
 }
 
 func (l *LocalGachaMasters) List(c echo.Context, h *Handler, requestAt int64) ([]*GachaData, error) {
-	ctx := c.Request().Context()
-
-	gachaMasterList := []*GachaMaster{}
-	query := "SELECT * FROM gacha_masters WHERE start_at <= ? AND end_at >= ? ORDER BY display_order ASC"
-	err := h.DB.SelectContext(ctx, &gachaMasterList, query, requestAt, requestAt)
-	if err != nil {
-		return nil, errorResponse(c, http.StatusInternalServerError, err)
+	var gachaMasterList []*GachaMaster
+	for _, gachaMaster := range l.GachaMasters {
+		if !(gachaMaster.StartAt <= requestAt && gachaMaster.EndAt >= requestAt) {
+			continue
+		}
+		gachaMasterList = append(gachaMasterList, gachaMaster)
 	}
 
 	if len(gachaMasterList) == 0 {
@@ -46,12 +67,11 @@ func (l *LocalGachaMasters) List(c echo.Context, h *Handler, requestAt int64) ([
 
 	// ガチャ排出アイテム取得
 	gachaDataList := make([]*GachaData, 0)
-	query = "SELECT * FROM gacha_item_masters WHERE gacha_id=? ORDER BY id ASC"
 	for _, v := range gachaMasterList {
 		var gachaItem []*GachaItemMaster
-		err = h.DB.SelectContext(ctx, &gachaItem, query, v.ID)
-		if err != nil {
-			return nil, errorResponse(c, http.StatusInternalServerError, err)
+		gachaItem, ok := l.GachaItemListByGachaID[v.ID]
+		if !ok {
+			return nil, errorResponse(c, http.StatusInternalServerError, fmt.Errorf("invalid gacha item (v.ID = %d)", v.ID))
 		}
 
 		if len(gachaItem) == 0 {
@@ -119,7 +139,7 @@ func (l *LocalGachaMasters) Pick(c echo.Context, h *Handler, gachaID string, gac
 // POST /gacha/refresh
 func (h *Handler) refreshGacha(c echo.Context) error {
 	//TODO for update master
-	err := localGachaMasters.Refresh()
+	err := localGachaMasters.Refresh(c, h)
 	if err != nil {
 		return errorResponse(c, http.StatusInternalServerError, ErrGetRequestTime)
 	}
